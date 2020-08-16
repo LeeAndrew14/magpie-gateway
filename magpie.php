933a3422f749d84007bab6cca612622a12c6e87a
Tukloy <rheyvince.26@gmail.com>
2020-08-16 08:41:57 +0800

    Manage version control

--- magpie.php
+++ magpie.php
@@ -6,30 +6,29 @@
  * Author: Code Disruptors Inc.
  * Author URI: https://github.com/LeeAndrew14
  * Version: 1.0.1
- *
-
-/*
- * Registers Magpie class as a WooCommerce payment gateway
  */
 
+// Block direct access to php files
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
-require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-
-include_once( 'magpie-rewrite-rule.php' );
+// Include database init function
 include_once( 'magpie-db-init.php');
-include_once( 'magpie-db-con.php' );
+include_once( 'magpie-rewrite-rule.php');
 
+// Register Magpie database init hook
+register_activation_hook( __FILE__, 'magpie_db_init' );
+
+// Registers Magpie Gateway class as a WooCommerce payment gateway
 add_filter( 'woocommerce_payment_gateways', 'magpie_gateway_init' );
-function magpie_gateway_init( $gateways ) {
+function magpie_gateway_init( $gateway ) {
     if ( ! class_exists( 'WC_Payment_Gateway' ) || ! class_exists( 'WC_Payment_Gateway' ) ) return;
 
-    // Include  Gateway Class
-    include_once( 'includes/magpie-gateway.php' );
-    include_once( 'includes/magpie-backend.php' );
+    // Include Magpie Gateway classes
     include_once( 'includes/magpie-api.php' );
+    include_once( 'includes/magpie-backend.php' );
+    include_once( 'includes/magpie-gateway.php' );
 
-    $gateways[] = 'WC_Magpie_Gateway';
+    $gateway[] = 'WC_Magpie_Gateway';
 
-	return $gateways;
+	return $gateway;
 }
