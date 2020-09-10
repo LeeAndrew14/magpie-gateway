181b04e88d025815e84616c5b9ff2830d3795678
Lee <lavpelegrina@gmail.com>
2020-09-10 16:46:44 +0800

    Add SG to allowed cards

--- includes/magpie-gateway.php
+++ includes/magpie-gateway.php
@@ -432,7 +432,9 @@ class WC_Magpie_Gateway extends WC_Payment_Gateway {
 
         // Do not allow international cards unless if in test mode
         if ( ! $this->test_mode ) {
-            if ( $card_token->card->country !== 'PH' ) {
+            $country_code = $card_token->card->country;
+
+            if ( $country_code !== 'PH' || $country_code !== 'SG' ) {
                 $message = 'Sorry, the country code of your card is not from the Philippines.
                     <br>We currently do not support international cards.
                     <br>Kindly try again or try using other payment methods.';
@@ -519,9 +521,6 @@ class WC_Magpie_Gateway extends WC_Payment_Gateway {
 
             $order->add_order_note( $message, false );
 
-            wc_add_notice( 'Something went wrong while processing your order. Please try again.
-                <br>If the problem persist please contact us.', 'error' );
-
             return;
         }
 
@@ -583,17 +582,11 @@ class WC_Magpie_Gateway extends WC_Payment_Gateway {
                 'Payment Failed ' . $charge_details->source->name . ' ' . wc_print_r( $charge_details, true )
             );
 
-            $order->update_status( 'pending' );
-
-            $redirect_message = $charge_details->redirect_response->message;
-
             $data['new_order_status'] = 'failed';
-
-            wc_reduce_stock_levels( $order_id );
     
             $magpie_backend->update_order_status( $data );
 
-            $order->add_order_note( $redirect_message, false );
+            wp_delete_post( $order_id, true );
 
             wp_redirect( wc_get_checkout_url() );
         }
